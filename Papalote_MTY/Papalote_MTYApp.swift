7593b89c604d273c38c6650626a56d2a807a2525
Alejandra Coeto <a01285221@tec.mx>
2024-10-14 19:37:18 -0600

    Added xcode project and gitignore

---
+++ Papalote_MTY/Papalote_MTYApp.swift
@@ -0,0 +1,17 @@
+//
+//  Papalote_MTYApp.swift
+//  Papalote_MTY
+//
+//  Created by Alejandra Coeto on 14/10/24.
+//
+
+import SwiftUI
+
+@main
+struct Papalote_MTYApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

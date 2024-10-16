62e791bd14195d60aef7034ff788773ccc1efb5d
joalgueflo <114319054+joalgueflo@users.noreply.github.com>
2024-10-15 23:22:56 -0600

    Created landingView file and added background video

---
+++ Papalote_MTY/Views/HomeView/LandingView.swift
@@ -0,0 +1,18 @@
+//
+//  LandingView.swift
+//  Papalote_MTY
+//
+//  Created by José Guerrero  on 15/10/24.
+//
+
+import SwiftUI
+
+struct LandingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LandingView()
+}

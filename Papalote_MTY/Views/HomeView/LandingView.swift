f026f16c04f37a7ea24b96c16392d3bb184cef4f
joalgueflo <114319054+joalgueflo@users.noreply.github.com>
2024-10-17 19:10:27 -0600

    Added video background to LandingView and Fonts

--- Papalote_MTY/Views/HomeView/LandingView.swift
+++ Papalote_MTY/Views/HomeView/LandingView.swift
@@ -7,12 +7,38 @@
 
 import SwiftUI
 
+func viewFonts() {
+        for family in UIFont.familyNames {
+            print(family)
+            for name in UIFont.fontNames(forFamilyName: family) {
+                print(name)
+            }
+        }
+    }
+
 struct LandingView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-        
-        
-        
+        ZStack {
+            
+            ZStack {
+                LoopingPlayerView(fileName: "PapaloteBackground")
+                
+                Color.black
+                    .opacity(0.40)
+            }
+            .ignoresSafeArea()
+            
+            VStack {
+                Text("Bienvenido a Papalote Museo Del Niño")
+                    .font(.system(size: 48, weight: .bold, design: .rounded))
+                    .multilineTextAlignment(.center)
+                    .foregroundColor(.white)
+                    .shadow(radius: 20)
+            }
+            .onAppear{
+                viewFonts()
+            }
+        }
     }
 }
 
